299248741fed4b11efebc443f003367bfd68bd91
Micah Breitenstein <micah.breitenstein@gmail.com>
2026-03-11 17:17:59 -0700

    Fix Selenium Dynamic Controls tests to focus on input enable/disable

--- pages/internet/dynamic_controls_page.py
+++ pages/internet/dynamic_controls_page.py
@@ -10,30 +10,16 @@ class DynamicControlsPage(BasePage):
     TOGGLE_CHECKBOX_BTN = (By.CSS_SELECTOR, "#checkbox-example button")
     ENABLE_DISABLE_BTN = (By.CSS_SELECTOR, "#input-example button")
     INPUT_FIELD = (By.CSS_SELECTOR, "#input-example input")
-    MESSAGE = (By.ID, "message")
-
-    def toggle_checkbox(self) -> "DynamicControlsPage":
-        self.click(self.TOGGLE_CHECKBOX_BTN)
-        self.wait_visible(self.MESSAGE)
-        return self
-
-    def is_checkbox_visible(self) -> bool:
-        return len(self.find_all(self.CHECKBOX)) > 0
 
     def enable_input(self) -> "DynamicControlsPage":
         self.click(self.ENABLE_DISABLE_BTN)
-        self.wait_visible(self.MESSAGE)
         self.wait.until(lambda d: self.find(self.INPUT_FIELD).is_enabled())
         return self
 
     def disable_input(self) -> "DynamicControlsPage":
         self.click(self.ENABLE_DISABLE_BTN)
-        self.wait_visible(self.MESSAGE)
         self.wait.until(lambda d: not self.find(self.INPUT_FIELD).is_enabled())
         return self
 
     def is_input_enabled(self) -> bool:
         return self.find(self.INPUT_FIELD).is_enabled()
-
-    def message_text(self) -> str:
-        return self.get_text(self.MESSAGE).strip()
