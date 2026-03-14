2a86ef2e9d5ef398d01cb1f136e710f941dac595
Micah Breitenstein <micah.breitenstein@gmail.com>
2026-03-13 18:26:34 -0700

    test(selenium): add geofence radius assertion for mocked location

--- tests/internet/test_geolocation.py
+++ tests/internet/test_geolocation.py
@@ -1,6 +1,10 @@
 import pytest
 
 from pages.internet import GeolocationPage
+from tests.internet.geospatial import (
+    haversine_km,
+    parse_coordinate,
+)
 
 
 CITIES = [
@@ -28,3 +32,23 @@ def test_geolocation_shows_mocked_coords_for_city(
 
     assert str(latitude) in lat_text
     assert str(longitude) in long_text
+
+
+@pytest.mark.no_safari
+def test_geolocation_is_within_2km_of_mocked_point(driver, base_url, mock_geolocation):
+    latitude = 37.7749
+    longitude = -122.4194
+
+    mock_geolocation(latitude, longitude)
+    page = (
+        GeolocationPage(driver, base_url=base_url)
+        .open()
+        .click_where_am_i()
+        .wait_for_coordinates()
+    )
+
+    actual_lat = parse_coordinate(page.lat_text())
+    actual_long = parse_coordinate(page.long_text())
+
+    distance_km = haversine_km(latitude, longitude, actual_lat, actual_long)
+    assert distance_km <= 2.0
