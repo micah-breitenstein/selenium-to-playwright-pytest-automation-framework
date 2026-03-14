d9958ae9215333df426f77dcb752367840c65729
Micah Breitenstein <micah.breitenstein@gmail.com>
2026-03-13 18:27:59 -0700

    test(selenium): add coordinate-range validation for geolocation output

--- tests/internet/test_geolocation.py
+++ tests/internet/test_geolocation.py
@@ -2,6 +2,7 @@ import pytest
 
 from pages.internet import GeolocationPage
 from tests.internet.geospatial import (
+    coordinates_in_valid_range,
     haversine_km,
     parse_coordinate,
 )
@@ -52,3 +53,26 @@ def test_geolocation_is_within_2km_of_mocked_point(driver, base_url, mock_geoloc
 
     distance_km = haversine_km(latitude, longitude, actual_lat, actual_long)
     assert distance_km <= 2.0
+
+
+@pytest.mark.no_safari
+def test_geolocation_coordinates_are_in_valid_numeric_ranges(
+    driver, base_url, mock_geolocation
+):
+    latitude = 47.6062
+    longitude = -122.3321
+
+    mock_geolocation(latitude, longitude)
+    page = (
+        GeolocationPage(driver, base_url=base_url)
+        .open()
+        .click_where_am_i()
+        .wait_for_coordinates()
+    )
+
+    actual_lat = parse_coordinate(page.lat_text())
+    actual_long = parse_coordinate(page.long_text())
+
+    assert coordinates_in_valid_range(actual_lat, actual_long)
+
+
